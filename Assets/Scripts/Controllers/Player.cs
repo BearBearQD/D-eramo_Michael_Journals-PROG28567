ec0948f86d3f62a8311ffff98498dbef3ec9e810
BearBearQD <86070469+BearBearQD@users.noreply.github.com>
2025-09-15 20:00:50 -0400

    Finished Task 1 A
    We did it in class

---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.B))
+        {
+            SpawnBombAtOffset(new Vector3(0, 1));
+        }
+
+        void SpawnBombAtOffset(Vector3 inOffset)
+        {
+            Vector2 spawnPosition = transform.position + inOffset;
+
+            Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+        }
+
+
+    }
+}
