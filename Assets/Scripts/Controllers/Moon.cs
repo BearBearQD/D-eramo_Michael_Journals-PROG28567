203a83ce6b26db5b2d8f99460bb013b06f76ac60
BearBearQD <86070469+BearBearQD@users.noreply.github.com>
2025-10-04 19:22:45 -0400

    DONE THE LAST TASK

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.Rendering;
 
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
+    public float radius = 4;
+    public float speed = 1;
+    public Transform target;
+
+    private float currentangle = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        OrbitalMotion(radius, speed,target);
+    }
+    void OrbitalMotion(float radius, float speed, Transform target)
+    {   
+        Vector2 from = target.position;
+        Vector2 to = transform.position;
+        currentangle += speed * Time.deltaTime;
+        float x = target.position.x+Mathf.Cos(currentangle)*radius;
+        float y = target.position.y+Mathf.Sin(currentangle)*radius;
+        transform.position = new Vector2(x, y);
+        print(currentangle*Mathf.PI);
     }
 }
